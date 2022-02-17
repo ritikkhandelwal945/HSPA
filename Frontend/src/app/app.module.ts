503bfe31010f9ab861289e0c1ba0326b925081f1
ritik.khandelwal945 <ritik.khandelwal945@outlook.com>
2022-02-17 18:08:51 +0530

    hspa

--- Frontend/src/app/app.module.ts
+++ Frontend/src/app/app.module.ts
@@ -1,15 +1,16 @@
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { PropertyListComponent } from './property/property-list/property-list.component';
 import { PropertyCardComponent } from './property/property-card/property-card.component';
-import { PropertyComponent } from './property-list/property/property.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    PropertyListComponent,
     PropertyCardComponent,
-    PropertyComponent
+    NavBarComponent
   ],
   imports: [
     BrowserModule
